cdf3c1da0466eca6849b0ab25e039a5e682f6495
apelbeku <syauqyalhaq5@gmail.com>
2020-08-10 17:20:34 +0700

    deploy update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Push Server PasswordW1pi
+                    Push Server Pondok
                 </div>
 
                 <div class="links">
